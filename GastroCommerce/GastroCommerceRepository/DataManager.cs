804689bf393bcf6c5df3fd19d66d14ceceff3ded
ezequiel r <eze1913@live.com.ar>
2017-11-08 22:35:41 -0300

    1 Commit =)

---
+++ GastroCommerce/GastroCommerceRepository/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GastroCommerceRepository
+{
+    public class DataManager
+    {
+        public static string ConnectionString { get; set; }
+
+        public DataManager()
+        {
+
+        }
+    }
+}
